316b7a5091bdf84fd2af7e1ecd35dbdf7f9beab0
森羅Mage <56330595+ShinraMage@users.noreply.github.com>
2024-04-30 18:28:58 +0800

    Update upload.ps1

--- upload.ps1
+++ upload.ps1
@@ -1,3 +1,7 @@
+cd page
+./upload.bat
+cd ../
+
 git config --local user.name "ShinraMage"
 git config --local user.email "shinra@posetmage.com"
 git remote set-url origin git@SH:ShinraMage/shinra_layouts.git
